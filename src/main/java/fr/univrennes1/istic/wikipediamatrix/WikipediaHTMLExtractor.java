62491604898cf13971ba22505ef06460e9eead17
odysseu <55583423+odysseu@users.noreply.github.com>
2023-08-08 17:52:02 +0200

    added box-RedirectBox plainlinks metadata ambox ambox-notice

--- src/main/java/fr/univrennes1/istic/wikipediamatrix/WikipediaHTMLExtractor.java
+++ src/main/java/fr/univrennes1/istic/wikipediamatrix/WikipediaHTMLExtractor.java
@@ -18,6 +18,7 @@ public class WikipediaHTMLExtractor {
 			"wikitable center", "multicol", "infobox wikitable", "wikitable sortable mw-collapsible mw-collapsed", "vgr-aggregators wikitable mw-collapsible-content", "vgr-reviews wikitable mw-collapsible-content", "vgr-awards wikitable mw-collapsible-content",
 			"sortable wikitable plainrowheaders" };
 	public static String[] ignoreType = {
+			"box-RedirectBox plainlinks metadata ambox ambox-notice",
 			"plainlinks fmbox fmbox-system",
 			"box-RfD plainlinks metadata ambox ambox-delete",
 			"box-Lead_missing plainlinks metadata ambox ambox-content ambox-lead_missing",
